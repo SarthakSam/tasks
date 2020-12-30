5b4a73bee3310560d1481e97573770a3edb8436c
Sarthak <sarthak242@gmail.com>
2020-12-30 21:16:18 +0530

    Feat: list button working

--- public/input.js
+++ public/input.js
@@ -3,36 +3,54 @@ const inputBtn = document.querySelector("section.container header.input-header .
 const inputTab = document.querySelector("section.container header.input-header .input-tab");
 const listBtn = document.querySelector("#list-button");
 const imageBtn = document.querySelector("#image-button");
+const listContainer =  document.querySelector("section.container header.input-header .input-tab ul.input-list");
+const descriptionContainer = document.querySelector("section.container header.input-header .input-tab > p.description");
+
+inputBtn.style.display = "inherit";
+inputTab.style.display = "none";
 
 document.addEventListener('click', (event) => {
     if( inputHeader.contains(event.target) ) {
-            inputTab.style.display = "block";
-            inputBtn.style.display = "none";
+            if( inputTab.style.display == "none") {
+                inputTab.style.display = "inherit";
+                inputBtn.style.display = "none";
 
-            if(event.target == listBtn) {
-               addList();
+                if(event.target == listBtn) {
+                addList();
+                }
+
+                else if(event.target == imageBtn) {
+                        addImage();
+                }
+                else {
+                        addDescription();
+                }
             }
-
-            else if(event.target == imageBtn) {
-                addImage();
-             }
-
-             else {
-                
-             }
 
     } 
     else {
-            inputBtn.style.display = "block";
+            inputBtn.style.display = "inherit";
             inputTab.style.display = "none";
     }
     
 })
 
+function clearDisplay() {
+        listContainer.style.display = "none";
+        descriptionContainer.style.display = "none";
+}
+
 function addList() {
-        console.log("list added");
+        clearDisplay();
+        listContainer.style.display = "initial";
 }
 
 function addImage() {
+        clearDisplay();
         console.log("image added");
+}
+
+function addDescription() {
+        clearDisplay();
+        descriptionContainer.style.display = "initial";
 }

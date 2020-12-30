3194bbbb73e3ba15c5c226b470ba4c60182159f2
Sarthak <sarthak242@gmail.com>
2020-12-30 20:42:56 +0530

    Style: lists css added

--- public/input.js
+++ public/input.js
@@ -1,15 +1,38 @@
 const inputHeader = document.querySelector("section.container header.input-header");
 const inputBtn = document.querySelector("section.container header.input-header .input-tab-btn");
 const inputTab = document.querySelector("section.container header.input-header .input-tab");
-
+const listBtn = document.querySelector("#list-button");
+const imageBtn = document.querySelector("#image-button");
 
 document.addEventListener('click', (event) => {
     if( inputHeader.contains(event.target) ) {
             inputTab.style.display = "block";
             inputBtn.style.display = "none";
+
+            if(event.target == listBtn) {
+               addList();
+            }
+
+            else if(event.target == imageBtn) {
+                addImage();
+             }
+
+             else {
+                
+             }
+
     } 
     else {
             inputBtn.style.display = "block";
             inputTab.style.display = "none";
     }
+    
 })
+
+function addList() {
+        console.log("list added");
+}
+
+function addImage() {
+        console.log("image added");
+}

c13aad8c4ca7268e9ec25ab97d3a67aabdf5b73d
Sarthak <sarthak242@gmail.com>
2020-12-31 14:33:21 +0530

    Feat: upload images func completed style: nav getting covered by content fixed

--- public/input.js
+++ public/input.js
@@ -5,6 +5,8 @@ const listBtn = document.querySelector("#list-button");
 const imageBtn = document.querySelector("#image-button");
 const listContainer =  document.querySelector("section.container header.input-header .input-tab ul.input-list");
 const descriptionContainer = document.querySelector("section.container header.input-header .input-tab > p.description");
+const uploadBtn = document.querySelector("section.container header.input-header .input-tab  div.images-section form #myFile");
+const uploadedImagesList = document.querySelector("section.container header.input-header .input-tab div.images-section ul.uploaded-images-list");
 
 inputBtn.style.display = "inherit";
 inputTab.style.display = "none";
@@ -31,6 +33,7 @@ document.addEventListener('click', (event) => {
     else {
             inputBtn.style.display = "inherit";
             inputTab.style.display = "none";
+        //     resetInputTab();
     }
     
 })
@@ -47,6 +50,7 @@ function addList() {
 
 function addImage() {
         clearDisplay();
+        descriptionContainer.style.display = "initial";
         console.log("image added");
 }
 
@@ -54,3 +58,26 @@ function addDescription() {
         clearDisplay();
         descriptionContainer.style.display = "initial";
 }
+
+uploadBtn.onchange = (event) => {
+        console.log("uloaded");
+        readURL(event.target)
+}
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                
+                reader.onload = function(e) {
+                        console.log(e.target.result);
+                        const imageContainer = document.createElement("li");
+                        const image = document.createElement("img");
+                        image.setAttribute('src', e.target.result);
+                        image.classList.add("uploaded-image")
+                        imageContainer.appendChild(image);
+                        uploadedImagesList.append(imageContainer);
+                }
+                
+                reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+        }        

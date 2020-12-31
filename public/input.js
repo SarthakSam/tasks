dc671c6dedc839896ad421c4b80824fefdad1e54
Sarthak <sarthak242@gmail.com>
2020-12-31 20:28:15 +0530

    Feat: image and list icons working

--- public/input.js
+++ public/input.js
@@ -6,7 +6,20 @@ const imageBtn = document.querySelector("#image-button");
 const listContainer =  document.querySelector("section.container header.input-header .input-tab ul.input-list");
 const descriptionContainer = document.querySelector("section.container header.input-header .input-tab > p.description");
 const uploadBtn = document.querySelector("section.container header.input-header .input-tab  div.images-section form #myFile");
+const uploadBtnLabel = document.querySelector("section.container header.input-header .input-tab  div.images-section form label.upload-btn");
 const uploadedImagesList = document.querySelector("section.container header.input-header .input-tab div.images-section ul.uploaded-images-list");
+const titleContainer = document.querySelector("section.container header.input-header .input-tab > p");
+const buttonsTray = document.querySelector("section.container header.input-header .input-tab .buttons-tray ul");
+
+const buttonsTrayFunctions = {
+        notify,
+        addUser,
+        changeColor,
+        addImage,
+        addList,
+        showOptions
+}
+
 
 inputBtn.style.display = "inherit";
 inputTab.style.display = "none";
@@ -31,6 +44,7 @@ document.addEventListener('click', (event) => {
 
     } 
     else {
+            clearDisplay();
             inputBtn.style.display = "inherit";
             inputTab.style.display = "none";
         //     resetInputTab();
@@ -41,22 +55,33 @@ document.addEventListener('click', (event) => {
 function clearDisplay() {
         listContainer.style.display = "none";
         descriptionContainer.style.display = "none";
+        uploadBtnLabel.style.display = "none";
+        descriptionContainer.innerText = "";
+        listContainer.innerHTML = "";
+        uploadedImagesList.innerHTML = "";
+        titleContainer.innerText = "";
 }
 
-function addList() {
-        clearDisplay();
-        listContainer.style.display = "initial";
-}
+// function addList() {
+//         addListItem();
+//         listContainer.style.display = "inherit";
+// }
 
-function addImage() {
-        clearDisplay();
-        descriptionContainer.style.display = "initial";
-        console.log("image added");
+function addListItem() {
+        const li = document.createElement("li");
+        li.classList.add("list-item");
+        const i = document.createElement("i");
+        i.classList.add("fa");
+        i.classList.add("fa-plus");
+        const p = document.createElement("p");
+        p.setAttribute("contenteditable", true);
+        li.appendChild(i);
+        li.appendChild(p);
+        listContainer.appendChild( li )
 }
 
 function addDescription() {
-        clearDisplay();
-        descriptionContainer.style.display = "initial";
+        descriptionContainer.style.display = "inherit";
 }
 
 uploadBtn.onchange = (event) => {
@@ -80,4 +105,46 @@ function readURL(input) {
                 
                 reader.readAsDataURL(input.files[0]); // convert to base64 string
         }
-        }        
+}        
+
+listContainer.addEventListener("keyup", (event) => {
+        if( event.target.parentElement == listContainer.lastElementChild && event.target.innerText.length == 1) {
+                addListItem();
+        }
+});
+
+buttonsTray.addEventListener('click', (event) => {
+        if(event.target.nodeName == "I") {
+                buttonsTrayFunctions[event.target.getAttribute("data-value")]();
+        }
+})
+
+function  notify() {
+        console.log("notified");
+}
+
+function  addUser() {
+        console.log("added user");
+}
+
+function  changeColor() {
+        console.log("changed color")
+}
+
+function  addImage() {
+        listContainer.style.display = "none";
+        descriptionContainer.style.display = "inherit";
+        uploadBtnLabel.style.display = "inherit";
+}
+
+function  addList() {
+        descriptionContainer.innerText = "";
+        descriptionContainer.style.display = "none";
+        listContainer.innerHTML = "";
+        addListItem();
+        listContainer.style.display = "inherit";
+}
+
+function  showOptions() {
+        console.log("ptiions")
+}

f86257e99b0a4fc53f6270273813e3bec55c84c2
Sarthak <sarthak242@gmail.com>
2021-01-29 17:27:42 +0530

    Feat: common-popup added and working on labels popup

--- public/js/labels.js
+++ public/js/labels.js
@@ -1,15 +1,35 @@
-import { getData } from './index.js';
+// import { getData } from './index.js';
 
 const labelPopupButton = document.querySelector(".navbar .nav-list .fa-tags");
 
-export function renderLabelsPopup() {
-    // getData('labels')
-    // .then( res => res.json() )
-    // .then( res => console.log(res));
-    let labels = localStorage.getItem("labels");
-
+labelPopupButton.onclick = () => {
+    renderLabelsPopup();
 }
 
-labelPopupButton.onclick = () => {
-    console.log("opend");
+window.onload = () => {
+    if(window.location.hash == '#labels')
+        renderLabelsPopup();
+}
+
+function renderLabelsPopup() {
+    const popupBody = document.querySelector("#labels > .popup > .popup-body");
+    const popupFooter = document.querySelector("#labels > .popup > .popup-footer");
+    // popup.innerHTML = "<p class='title'> Edit Labels </p>";
+    // popup.appendChild( newLabelElement() );
+    // popup.appendChild( existingLabels() );
+    popupFooter.innerHTML = "<button class = 'close-popup-btn'>Done</button>";
+}
+
+function newLabelElement() {
+    const div = document.createElement("div");
+    div.innerHTML = "<i class='fas fa-plus'></i><input type='text'>"
+    return div;
+}
+
+function existingLabels() {
+    let labels = localStorage.getItem("labels");
+    const fragment = document.createDocumentFragment();
+    labels.forEach( label => {
+        const li = document.createElement("li");
+    } );
 }
